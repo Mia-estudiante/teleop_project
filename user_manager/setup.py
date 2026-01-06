39d928c68ad383c228fc95ef49bf56beb53eb3d7
Mia-estudiante <eunae.engineer@gmail.com>
2026-01-06 15:40:55 +0900

    [Add] stream image

--- user_manager/setup.py
+++ user_manager/setup.py
@@ -26,7 +26,6 @@ setup(
         'console_scripts': [
             'user_data_node = user_manager.user_data_node:main',
             'avp_node = user_manager.avp_node:main',
-            'test_node = user_manager.test_image:main'
         ],
     },
 )
--- user_manager/user_manager/avp_node.py
+++ user_manager/user_manager/avp_node.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Pose
+from sensor_msgs.msg import Image
 
 # pinocchio
 import pinocchio as pin
@@ -9,17 +10,37 @@ from pinocchio.utils import *
 
 from avp_stream.streamer import VisionProStreamer
 
+import cv2
+from cv_bridge import CvBridge
+
+bridge = CvBridge()
+
 class AVPNode(Node):
     def __init__(self):
         super().__init__('avp_node')
         self.get_logger().info('AVP Node has been started.')
 
+        self.default_fps = 30
+        self.default_disparity = 30
+        self.default_resolution = "640x480" # same with the mjc renderer
+        self.frame = np.ones((480,640,3))
         self.streamer = VisionProStreamer(ip="192.168.123.5")
+        # Register stereo frame callback with custom disparity scale
+        self.streamer.register_frame_callback(self.create_rgb_visualizer)
+        self.streamer.configure_video(
+            fps=self.default_fps,
+            size=self.default_resolution,     # Side-by-side stereo resolution
+            stereo=True,              # Enable stereo video mode
+        )
+        self.streamer.start_webrtc(port=9999)
+        # self.streamer.start_streaming()
+
         self.latest = None
 
         self.head_publisher = self.create_publisher(Pose, '/head', 10)
         self.lwrist_publisher = self.create_publisher(Pose, '/lwrist', 10)
         self.rwrist_publisher = self.create_publisher(Pose, '/rwrist', 10)
+        self.camera_subscriber = self.create_subscription(Image, '/mujoco/camera', self.camera_callback, 10)
         # self.lfingers_publisher = self.create_publisher(Pose, '/lfingers', 10)
         # self.rfingers_publisher = self.create_publisher(Pose, '/rfingers', 10)
 
@@ -90,6 +111,15 @@ class AVPNode(Node):
         wrist_msg.orientation.w = xyzquat[6]
         self.rwrist_publisher.publish(wrist_msg)
             
+    def camera_callback(self, msg: Image):
+        frame = bridge.imgmsg_to_cv2(msg)
+        self.frame = np.fliplr(np.flipud(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)))
+
+    def create_rgb_visualizer(self, blank_frame):
+        """Visualize streaming video"""
+        np.copyto(blank_frame, self.frame)
+        return blank_frame
+
 def main():
     rclpy.init()
     node = AVPNode()
